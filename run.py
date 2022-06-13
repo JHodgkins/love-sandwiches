7742fb2ed2b8bc7d62c2db88ba6854bfa055472a
JHodgkins <jhodgkins78@gmail.com>
2022-06-13 12:09:17 +0000

    code: get_sales_data function created to retrieve string

--- run.py
+++ run.py
@@ -11,7 +11,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches_cli_project')
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
+# sales = SHEET.worksheet('sales')
+# data = sales.get_all_values()
 
-print(data)
+# print(data)
+
+
+def get_sales_data():
+    """
+    Get sales data from user input through the terminal
+    """
+    print("Please enter the sales data from the last market day.")
+    print("Data should be 6 numbers seperated by commas ','")
+    print("Use the example below for reference.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Please enter your sales data numbers:")
+    print(f"The data provided was: {data_str}")
+
+
+get_sales_data()
