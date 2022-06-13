202b2690ccb78853d9a8a9feaa2e17fba4fac8d7
JHodgkins <jhodgkins78@gmail.com>
2022-06-13 12:56:57 +0000

    code: validation added to check entered data is equal to 6 numbers

--- run.py
+++ run.py
@@ -27,7 +27,24 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Please enter your sales data numbers:")
-    print(f"The data provided was: {data_str}")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
 
+
+def validate_data(value):
+    """
+    Validate data entered by the user
+    try converts all string values to int
+    ValueError will be raised if cannot convert to int or
+    data entered is not 6 numbers.
+    """
+    try:
+        if len(value) != 6:
+            raise ValueError(f"6 numbers are required, you entered {len(value)}")
+        # elif value != int:
+        #     raise ValueError(f"Numbers are only allowed to be entered, you entered {value}")
+    except ValueError as e:
+        print(f"Error: {e}, \nPlease re-enter the sales data\n")
 
 get_sales_data()
