3e8663afdc980f67523aed4551823a633923aedb
JHodgkins <jhodgkins78@gmail.com>
2022-06-14 16:09:28 +0000

    code: calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -71,7 +71,7 @@ def calculate_surplus_stock(sales_row):
         - Positive surplus indicates waste.
         - Negative surplus indicates, extra stock made when item was out of stock.
     """
-    print("Calculating surplus data...\n")
+    print("Calculating surplus data...")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
     surplus_data = []
@@ -93,6 +93,20 @@ def get_last_five_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """.git/
+    Calculate the average stock for each item type, 
+    adding an additional 10%
+    """
+    print("Calculating stock data")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+
 def main():
     """
     Run main programme functions
@@ -102,8 +116,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_stock(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches - Data managment tool \n")
-# main()
-sales_columns = get_last_five_entries_sales()
+main()
