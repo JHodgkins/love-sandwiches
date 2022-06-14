12db40afd2d421b424e503dccccbf9a28e302e39
JHodgkins <jhodgkins78@gmail.com>
2022-06-14 01:35:09 +0000

    refactor: update worksheet function to accept sheet variable

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+# from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,11 +11,6 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches_cli_project')
-
-# sales = SHEET.worksheet('sales')
-# data = sales.get_all_values()
-
-# print(data)
 
 
 def get_sales_data():
@@ -30,14 +25,13 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Please enter your sales data numbers:")
-        
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Sales data logged")
+            print("\nSales data logged")
             break
-    
     return sales_data
+
 
 def validate_data(values):
     """
@@ -52,20 +46,22 @@ def validate_data(values):
             raise ValueError(
                 f"Exactly 6 values are required, you provided {len(values)}"
             )
-    except ValueError as e:
-        print(f"Invalid entry: {e}\nPlease re-enter your sales data again.\n")
+    except ValueError as err:
+        print(f"Invalid entry: {err}\nPlease re-enter your sales data again.\n")
         return False
 
     return True
 
-def update_sales_data(data):
+
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, adds the new row of data entered by the user.
+    Data passed in will be used to add data to a specified worksheet
+    worksheet variable will desiginate which worksheet to update.
     """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully!.\n")
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully!.\n")
 
 
 def calculate_surplus_stock(sales_row):
@@ -73,29 +69,17 @@ def calculate_surplus_stock(sales_row):
         Compare sales with stock and calculate the surplus for each item type.
         The surplus is defined as the sales figure subtracted from the stock:
         - Positive surplus indicates waste.
-        - Negative surplus indicates, extra stock made when item was out of stock. 
+        - Negative surplus indicates, extra stock made when item was out of stock.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
     return surplus_data
 
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, adds the new row of data calculated 
-    by the function calculate_surplus_stock.
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully!.\n")
 
 def main():
     """
@@ -103,9 +87,10 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_data(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_stock(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+
 
 print("Welcome to Love Sandwiches - Data managment tool \n")
 main()
